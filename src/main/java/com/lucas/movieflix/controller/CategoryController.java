d416afcc5e0fdebcfb80670d03778309d272c01d
Lucas <lucaspeixoto349@gmail.com>
2025-07-12 13:55:59 -0300

    refactor: replace Lombok constructor with @Autowired injection
    Replaced @RequiredArgsConstructor with explicit @Autowired constructors in CategoryController and CategoryService to fix compilation issues.

--- src/main/java/com/lucas/movieflix/controller/CategoryController.java
+++ src/main/java/com/lucas/movieflix/controller/CategoryController.java
@@ -2,24 +2,28 @@ package com.lucas.movieflix.controller;
 
 import com.lucas.movieflix.entity.Category;
 import com.lucas.movieflix.service.CategoryService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController()
+@RestController
 @RequestMapping("/movieflix/category")
-@RequiredArgsConstructor
 public class CategoryController {
-
 
     private final CategoryService categoryService;
 
-    @GetMapping()
-    public List<Category> getAllCategories(){
+    @Autowired
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public List<Category> getAllCategories() {
         return categoryService.findAll();
     }
 
 }
+
--- src/main/java/com/lucas/movieflix/service/CategoryService.java
+++ src/main/java/com/lucas/movieflix/service/CategoryService.java
@@ -2,18 +2,21 @@ package com.lucas.movieflix.service;
 
 import com.lucas.movieflix.entity.Category;
 import com.lucas.movieflix.repository.CategoryRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 public class CategoryService {
 
     private final CategoryRepository repository;
 
-    public List<Category> findAll(){
+    public CategoryService(CategoryRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Category> findAll() {
         return repository.findAll();
     }
 }
+
